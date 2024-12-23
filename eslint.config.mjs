e1bd3ecf03f25ed0a0ae324a4b05663d003485ca
Quinton Urquhart <63878395+qurquhart@users.noreply.github.com>
2024-12-23 15:34:47 -0700

    eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.rules({
+    "@typescript-eslint/no-explicit-any": "off",
+    "react/no-unescaped-entities": "off",
+  }),
 ];
 
 export default eslintConfig;
