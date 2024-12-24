0087faf5cf0d465717f070c73059eac30c5d0d36
Quinton Urquhart <63878395+qurquhart@users.noreply.github.com>
2024-12-24 02:12:27 -0700

    imports

--- app/gorldoku/page.tsx
+++ app/gorldoku/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { DeleteIcon } from "../Wordle";
+import { NumberButton, SodukoCell } from "../Doku";
 
 export default function page() {
   const sudoku = require("@/lib/sudoku.js");
@@ -131,66 +131,5 @@ export default function page() {
         </button>
       </div>
     </div>
-  );
-}
-
-export function NumberButton({
-  number,
-  onClick,
-}: {
-  number: string;
-  onClick: any;
-}) {
-  return (
-    <button
-      onClick={() => onClick(number)}
-      className="p-3 flex justify-center items-center border-2 rounded-lg border-black/30 bg-gray-100 text-xl font-semibold"
-    >
-      {number == "." ? <DeleteIcon /> : number}
-    </button>
-  );
-}
-
-export function SodukoCell({
-  index,
-  number,
-  onClick,
-  selected,
-  original,
-}: {
-  index: number;
-  number: string;
-  onClick: any;
-  selected: boolean;
-  original: boolean;
-}) {
-  if (original) {
-    return (
-      <div
-        className={`w-10 h-10 ${original ? "bg-gray-200 text-gray-800" : ""} ${
-          selected ? "bg-gray-100" : ""
-        } border border-black/50 flex items-center justify-center font-bold ${
-          index % 3 == 2 ? "border-r-4 border-r-black" : ""
-        } ${index > 17 && index < 27 ? "border-b-4 border-b-black" : ""} ${
-          index > 44 && index < 54 ? "border-b-4 border-b-black" : ""
-        }`}
-      >
-        {number != "." && number}
-      </div>
-    );
-  }
-  return (
-    <button
-      onClick={() => onClick(index)}
-      className={`w-10 h-10 ${original ? "bg-gray-200 text-gray-700" : ""} ${
-        selected ? "bg-yellow-100 " : ""
-      } border border-black/50 flex items-center justify-center font-bold ${
-        index % 3 == 2 ? "border-r-4 border-r-black" : ""
-      } ${index > 17 && index < 27 ? "border-b-4 border-b-black" : ""} ${
-        index > 44 && index < 54 ? "border-b-4 border-b-black" : ""
-      }`}
-    >
-      {number != "." && number}
-    </button>
   );
 }
